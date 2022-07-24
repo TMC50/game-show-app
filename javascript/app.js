b2e7863d39b57f4bdb62bfb25d08eff9a645b2d4
TMC50 <90803101+TMC50@users.noreply.github.com>
2022-07-23 19:57:49 -0700

    Updated app.js and index.html

---
+++ javascript/app.js
@@ -0,0 +1,22 @@
+const qwerty = document.getElementById('qwerty');
+const phrase = document.getElementById('phrase');
+let startButton = document.getElementsByClassName('.btn__reset');
+const missedGuesses = 0;
+
+// Five array strings to be used in the guessing game
+const phrases = [
+    'i love code',
+    'fly an airplane',
+    'the earth is round',
+    'candy is life',
+    'lasagna is amazing'
+];
+
+// Listens for the start game button to be selected
+startButton.addEventListener('click', () => {
+    
+});
+
+function getRandomPhraseAsArray() {
+    let numbArrayLength = '';
+}
